09ed8ba1771977e21168098231e041729e59a3c0
Ashiqur Rahman <rohan@Ashiqurs-MacBook-Air.local>
2024-04-02 17:20:28 +0600

    Add rack cors and create users#create endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize_user
+  def failed_response(message)
+    {
+      data: nil,
+      message: message,
+    }
+  end
+
+  protected
+
+  def authorize_user
+    header = request.headers['Authorization']
+    return render json: failed_response('Authentication error!'), status: :unauthorized unless header.present?
+
+    token = header.split(' ').last
+    begin
+      @current_user = User.find_by(auth_token: token)
+      unless @current_user.present?
+        render json: failed_response('Authentication error!'), status: :unauthorized
+      end
+    end
+  end
 end
